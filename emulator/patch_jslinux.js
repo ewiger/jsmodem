528ce1af2f1cd3b216b7aad320afbd579a3ae972
Yauhen Yakimovich <yauhen.yakimovich@uzh.ch>
2011-10-11 00:10:41 +0200

    Added jslinux files from bellard.org to make project independent of new changes. Right now it is in sync with version from Aug 20, 2011. Wget commands and patching is left in Makefile as the fallback.
    new file:   cpux86-ta.js
    new file:   cpux86.js
    new file:   jslinux.js
    modified:   patch_jslinux.js
    new file:   term.js
    renamed:    vmlinux26.bin -> vmlinux-2.6.20.bin

--- emulator/patch_jslinux.js
+++ emulator/patch_jslinux.js
@@ -1,13 +1,12 @@
---- a/jslinux.js	2011-05-26 22:06:19.000000000 +0200
-+++ b/jslinux.js	2011-06-26 16:32:58.644879002 +0200
-@@ -69,6 +69,10 @@
-     params.get_boot_time = get_boot_time;
+--- a/jslinux.js	2010-10-09 00:00:00.000000000 +0200
++++ b/jslinux.js	2011-10-11 00:07:55.617425392 +0200
+@@ -70,6 +70,9 @@
  
      pc = new PCEmulator(params);
-+    
+ 
 +    // Add JSModem for networking.
 +    var modem = new JSModem(pc);
 +    modem.connect('localhost', 2080);
+     pc.load_binary("vmlinux-2.6.20.bin", 0x00100000);
  
-     pc.load_binary("vmlinux26.bin", 0x00100000);
-
+     initrd_size = pc.load_binary("root.bin", 0x00400000);
